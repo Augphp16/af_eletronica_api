0b99f9f54de231d3fc47fd3720cda27745f54039
Augphp16 <ferreiraaugusto62@gmail.com>
2024-08-22 21:07:51 -0300

    Subindo arquivos da api

---
+++ src/main/java/br/com/api/af_eletronica_api/AfEletronicaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.af_eletronica_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfEletronicaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfEletronicaApiApplication.class, args);
+	}
+
+}
